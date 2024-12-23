27f6634a4b5a1a4d387754a7dd3600a5ff0245cd
vedant8767 <155797741+vedant8767@users.noreply.github.com>
2024-12-23 23:57:19 +0530

    responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,54 @@
-import React from 'react'
-import '../css/Navbar.css'
+// import React from 'react'
+// import '../css/Navbar.css'
+
+// function Navbar() {
+//   return (
+//     <div className='navbar'>
+//         <div className="logo">SVS <span>Technology</span></div>
+//         <div className="nav_elements">
+//            <ul>
+//             <li><a href="#home">Home</a></li>
+//             <li><a href="#service">Services</a></li>
+//             <li><a href="#project">Projects</a></li>
+//             <li><a href="#contact"><button>Contact Us Now</button></a></li>
+//            </ul>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default Navbar
+import React, { useState } from 'react';
+import '../css/Navbar.css';
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
-    <div className='navbar'>
-        <div className="logo">SVS <span>Technology</span></div>
-        <div className="nav_elements">
-           <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#service">Services</a></li>
-            <li><a href="#project">Projects</a></li>
-            <li><a href="#contact"><button>Contact Us Now</button></a></li>
-           </ul>
-        </div>
+    <div className="navbar">
+      <div className="logo">SVS <span>Technology</span></div>
+      <div className={`nav_elements ${isMenuOpen ? 'open' : ''}`}>
+        <ul>
+          <li><a href="#home" onClick={closeMenu}>Home</a></li>
+          <li><a href="#service" onClick={closeMenu}>Services</a></li>
+          <li><a href="#project" onClick={closeMenu}>Projects</a></li>
+          <li><a href="#contact" onClick={closeMenu}><button>Contact Us Now</button></a></li>
+        </ul>
+      </div>
+      <div className="hamburger" onClick={toggleMenu}>
+        {isMenuOpen ? <span>&times;</span> : <span>&#9776;</span>}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
+
